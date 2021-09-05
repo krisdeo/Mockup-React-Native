2fddc8de62517ed634a8fcf06d2fb4e7640d6245
zimmeritstronk <89579161+zimmeritstronk@users.noreply.github.com>
2021-09-05 22:32:45 +0700

    MKP Mobile App

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+ import {AppRegistry} from 'react-native';
+ import App from './src/App';
+ import MainApp from './src/MainApp';
+ import {name as appName} from './app.json';
+ import CallApi from './src/CallApi';
+ 
+ AppRegistry.registerComponent(appName, () => MainApp);
+  
